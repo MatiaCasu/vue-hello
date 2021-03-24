9f3e2e438ae6133c05163bf58c4c2b29bc4b6369
Matia <matia.casu@gmail.com>
2021-03-24 15:22:35 +0100

    Stampato messaggio in h1 con Vue.Js data

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+const divContent = new Vue(
+  {
+    el: "#htmlElement",
+    data: {
+      text: "Titolone",
+    }
+  });
